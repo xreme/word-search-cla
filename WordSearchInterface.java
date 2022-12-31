4faae5c00a18b0cffa3337e9adaf3c336e92dfa9
OSEREME <94149337+xreme@users.noreply.github.com>
2022-12-31 14:39:52 -0500

    Update WordSearchInterface.java

--- WordSearchInterface.java
+++ WordSearchInterface.java
@@ -1,15 +1,12 @@
-
 import java.io.IOException;
 import java.util.Scanner;
-
-
+import javax.lang.model.util.ElementScanner14;
 
 public class WordSearchInterface{
 //this class handles the user input
     public static void main (String[] args){
+        System.out.print("\033[H\033[2J");
 
-        System.out.println("--------------------------------"
-        +"\nWELCOME: Enter 'Help' for help\n--------------------------------");
         //create the wordsearch game
         WordSearchGame wordSearch = new WordSearchGame();
         
@@ -22,66 +19,99 @@ public class WordSearchInterface{
             String action  = uInput.nextLine();
             if (action == null || action.equals("")) 
 			{
-				System.out.print("\n>");
+                wordSearch.refresh("");
+				//System.out.print("\n>");
 				continue;
 			}
+            //OPEN HELP DIALOGUE
+            else if (action.equalsIgnoreCase("HELP")){
+                String HelpString = "\n---Commands---"+
+                "\nQuit(q): Quit the game \n"+
+                "\nReset(r): Reset the word search with new words\n"+ 
+                "\nFind(f): Find a word, prompts user for the coordinates of \n         the first and last letter of a word\n" +
+                "\nPrint(p): Display the current word search page and words\n" +
+                "\nWords(w): Print out all the words in the wordsearch\n" +
+                "\nWords+(w+): Print out all the words, missing words, and found words\n";
+                
+                wordSearch.refresh(HelpString);
+            }
 			//QUIT THE THE PROGRAM
             else if (action.equalsIgnoreCase("Q") || action.equalsIgnoreCase("QUIT")){
                 return;
+            }
+            //Reset
+            else if (action.equalsIgnoreCase("R")||action.equalsIgnoreCase("Reset")){
+                wordSearch.clearScreen();
+                System.out.print("ARE YOU SURE YOU WANT TO RESET?(Y/N)\n>");
+                String confirm = uInput.nextLine();
+                if (confirm.equalsIgnoreCase("Y")) {
+                    wordSearch.clearScreen();
+                    wordSearch = new WordSearchGame();
+                    System.out.print(">");
+                }
+                else if (confirm.equalsIgnoreCase("N")){
+                    wordSearch.refresh("\nReset Cancelled");
+                }
+                else{
+                    wordSearch.refresh("\nInvalid Input");
+                }
             }
             //PRINTS THE CURRENT GAME BAORD AND WORDS
             else if (action.equalsIgnoreCase("Print")||action.equalsIgnoreCase("P")){
                 wordSearch.printGame();
             }
             //PRINTS THE WORDS IN THE WORD SEARCH
-            else if (action.equalsIgnoreCase("PrintWords")){
-                System.out.println(wordSearch.getWords());
+            else if (action.equalsIgnoreCase("WORDS")||action.equalsIgnoreCase("W")){
+                wordSearch.refresh("\nALL WORDS: "+wordSearch.getWords().toString());
             }
-            // PRINT THE WORDS SEARCH PAGE
-            else if (action.equalsIgnoreCase("PrintPage")){
-                System.out.println(wordSearch.getTableString());
-            }
-            //
-            else if (action.equalsIgnoreCase("WORD") || action.equalsIgnoreCase("W")){
-                System.out.println("ALL WORDS: "+ wordSearch.getWords()+"\n");
-                System.out.println("MISSING WORDS:" + wordSearch.getMissingWords()+"\n");
-                System.out.println("WORDS FOUND: " + wordSearch.getWordsFound()+"\n");
-            }
-            //OPEN HELP DIALOGUE
-            else if (action.equalsIgnoreCase("HELP")){
-                System.out.println("Quit(q): quit \nhe"+ 
-                "\nFind(f): Enables find mode, prompts user for word coordinates \n" +
-                "\nPrint(p): print out the board" +
-                "\nPrintWords: print out the words" +
-                "\nPrintPage: print out the page" +
-                "\nWord: print out all words, missing words and words found"
-                );
-            }
-            //MAKE A GUESS
-            else if(action.equalsIgnoreCase("FIND") || action.equalsIgnoreCase("F")){
-                // *******make some error cathing here, make sure that the input coords make sense! *******
-                System.out.println("FIND WORD \n");
-                System.out.println("Coordinates of First Letter:");
-                System.out.print("X: ");
-                int x1 = Integer.parseInt(uInput.nextLine());
-                System.out.print("Y: ");
-                int y1 =  Integer.parseInt(uInput.nextLine());
+            //PRINT ALL WORDS, MISSING WORDS, AND WORDS FOUND
+            else if (action.equalsIgnoreCase("WORDS+") || action.equalsIgnoreCase("W+")){
+                wordSearch.refresh("\nALL WORDS: \n"+ wordSearch.getWords()+"\n\nMISSING WORDS:\n" + wordSearch.getMissingWords()
+                +"\n\nWORDS FOUND: \n" + wordSearch.getWordsFound()+"\n");
                 
-                System.out.println("\n Coordinates of Last Letter:");
-                System.out.print("X: ");
-                int x2 = Integer.parseInt(uInput.nextLine());
-                System.out.print("Y: ");
-                int y2 =  Integer.parseInt(uInput.nextLine());
-
-                wordSearch.guessWord(x1, y1, x2, y2 );
-
-                wordSearch.printGame();
+               /*  System.out.println("ALL WORDS: "+ wordSearch.getWords()+"\n");
+                System.out.println("\nMISSING WORDS:" + wordSearch.getMissingWords()+"\n");
+                System.out.println("\nWORDS FOUND: " + wordSearch.getWordsFound()+"\n"); */
+            }
+            //FIND A WORD
+            else if(action.equalsIgnoreCase("FIND") || action.equalsIgnoreCase("F")){
+               
+                int x1,x2,y1,y2;
+                wordSearch.refresh("\nFIND WORD \n");              
+                try{
+                    System.out.println(" Coordinates of First Letter:");
+                    System.out.print("X: ");
+                    x1 = Integer.parseInt(uInput.nextLine());
+                    System.out.print("Y: ");
+                    y1 =  Integer.parseInt(uInput.nextLine());
+                    
+                    System.out.println("\n Coordinates of Last Letter:");
+                    System.out.print("X: ");
+                    x2 = Integer.parseInt(uInput.nextLine());
+                    System.out.print("Y: ");
+                    y2 =  Integer.parseInt(uInput.nextLine());
+                }
+                catch(java.lang.NumberFormatException exception){
+                    wordSearch.refresh("\nINVALID INPUT ERROR: Non-integer, \nPlease Input Integer(0-9)\n");
+                    continue;
+                }              
+                try{
+                    wordSearch.findWord(x1, y1, x2, y2);
+                }
+                catch (java.lang.ArrayIndexOutOfBoundsException exception){
+                    wordSearch.refresh("\nINVALID INPUT ERROR: Guess is out of bounds");
+                    continue;
+                }
             }
             //USER TUTORIAL
             else if (action.equalsIgnoreCase("TUTORIAL")){
                 System.out.print("This freature has not been implemented yet...");;
             }
-            System.out.print("\n>");
+            else{
+                wordSearch.refresh("\n Invalid Input");
+            }
+            continue;
+            //System.out.print("\n>");
         }
     }
 }
